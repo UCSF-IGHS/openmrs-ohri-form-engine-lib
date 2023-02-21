ff92cf6656f0354955679ed3e5f094aa9a5b3921
pirupius <piruville@gmail.com>
2023-02-21 12:28:24 +0300

    OHRI-1279 adds support for multiple post submission actions

--- src/hooks/usePostSubmissionAction.tsx
+++ src/hooks/usePostSubmissionAction.tsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { PostSubmissionAction } from '../api/types';
 import { getPostSubmissionActionById } from '../registry/registry';
 
-export function usePostSubmissionAction(actionId: string) {
+export function usePostSubmissionAction(actionIds: Array<string>) {
   const [action, setAction] = useState<PostSubmissionAction>(null);
   useEffect(() => {
-    if (actionId) {
-      getPostSubmissionActionById(actionId).then(response => setAction(response.default));
+    if (actionIds) {
+      actionIds.forEach(actionId => {
+        getPostSubmissionActionById(actionId).then(response => setAction(response.default));
+      });
     }
-  }, [actionId]);
+  }, [actionIds]);
 
   return action;
 }
--- src/ohri-form.component.tsx
+++ src/ohri-form.component.tsx
@@ -105,7 +105,7 @@ const OHRIForm: React.FC<OHRIFormProps> = ({
   const handlers = new Map<string, FormSubmissionHandler>();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [pagesWithErrors, setPagesWithErrors] = useState([]);
-  const postSubmissionHandler = usePostSubmissionAction(refinedFormJson?.postSubmissionAction);
+  const postSubmissionHandlers = usePostSubmissionAction(refinedFormJson?.postSubmissionActions);
   const [isLoadingFormDependencies, setIsLoadingFormDependencies] = useState(true);
 
   const sessionMode = useMemo(() => {
@@ -208,9 +208,9 @@ const OHRIForm: React.FC<OHRIFormProps> = ({
             });
           }
           // Post Submission Actions
-          if (postSubmissionHandler) {
+          if (postSubmissionHandlers) {
             await Promise.resolve(
-              postSubmissionHandler.applyAction({
+              postSubmissionHandlers.applyAction({
                 patient,
                 sessionMode,
                 encounters: results.map(encounterResult => encounterResult.data),
