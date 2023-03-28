b3c72bb6eb56bdbc24c19a07c076eecce54ae39c
Donald Kibet <chelashawdonald@yahoo.com>
2023-03-28 16:15:26 +0300

    (feat) add ability to use `ohri-form-engine` as an extension

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,37 @@ export * from './ohri-form-context';
 export * from './components/value/view/ohri-field-value-view.component';
 export * from './components/previous-value-review/previous-value-review.component';
 export { default as OHRIForm } from './ohri-form.component';
+
+import { defineConfigSchema, getAsyncLifecycle } from '@openmrs/esm-framework';
+
+declare var __VERSION__: string;
+// __VERSION__ is replaced by Webpack with the version from package.json
+const version = __VERSION__;
+
+const backendDependencies = {
+  'webservices.rest': '^2.2.0',
+  fhir2: '^1.2.0',
+};
+
+function setupOpenMRS() {
+  const moduleName = '@ohri/esm-form-engine-app';
+
+  const options = {
+    featureName: 'ohri-form-engine',
+    moduleName,
+  };
+
+  defineConfigSchema(moduleName, {});
+
+  return {
+    extensions: [
+      {
+        name: 'ohri-form-engine',
+        slot: 'ohri-form-engine-slot',
+        load: getAsyncLifecycle(() => import('./ohri-form.component'), options),
+      },
+    ],
+  };
+}
+
+export { backendDependencies, setupOpenMRS, version };
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,2 +1,4 @@
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-when';
+
+global.__VERSION__ = '1.0.0';
